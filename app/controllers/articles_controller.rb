1cf526126fdf4f01fffc663a0b0e953ae97dfd18
Kaolla <harlequin.crest@gmail.com>
2016-11-18 17:52:41 +0100

    Implement Edit, Create, Update actions and views + flash + refactor

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,15 +1,54 @@
 class ArticlesController < ApplicationController
 
+	before_action :find_article, only: [:show, :edit, :update, :destroy]
+
 	def index
 		@articles = Article.all
 	end
 
 	def show
-		@article = Article.find(params[:id])
 	end
 
 	def new
-		
+		@article = Article.new
+	end
+
+	def create
+		@article = Article.new(article_params)
+		@article.save
+
+		flash.notice = "Article #{@article.title} Created!"
+
+		redirect_to article_path(@article)
+	end
+
+	def destroy
+		@article.destroy
+
+		flash.notice = "Article #{article.title} Deleted!"
+
+		redirect_to articles_path
+	end
+
+	def edit
+	end
+
+	def update
+		@article.update(article_params)
+
+		flash.notice = "Article #{@article.title} Updated!"
+
+		redirect_to article_path(@article)
+	end
+
+	private 
+
+	def article_params
+		params.require(:article).permit(:title, :body)
+	end
+
+	def find_article
+		@article = Article.find(params[:id])
 	end
 
 end
